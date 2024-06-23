d1169c0ff1caf966ed061688e7fc0fd9b58bbb80
Quarshie <asi37375@hotmail.com>
2024-06-23 19:54:42 +0100

    footer js code added

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2024 PLATFORM FOR THE NEEDY. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
